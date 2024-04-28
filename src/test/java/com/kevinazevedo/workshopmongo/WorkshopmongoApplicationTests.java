1046294ad481e21a6dd4b1fa99e53dad075bd84b
kevinazevedo <taylorvision95@outlook.com>
2024-04-28 12:56:38 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/kevinazevedo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinazevedo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
