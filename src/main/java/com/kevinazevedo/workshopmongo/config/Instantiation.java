0aaab17e173f655b894fd0b5f85b7db31d2a35b5
kevinazevedo <taylorvision95@outlook.com>
2024-05-03 14:59:52 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/kevinazevedo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/kevinazevedo/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.kevinazevedo.workshopmongo.domain.Post;
 import com.kevinazevedo.workshopmongo.domain.User;
+import com.kevinazevedo.workshopmongo.dto.AuthorDTO;
 import com.kevinazevedo.workshopmongo.repository.PostRepository;
 import com.kevinazevedo.workshopmongo.repository.UserRepository;
 
@@ -32,10 +33,11 @@ public class Instantiation implements CommandLineRunner {
 		User mark = new User(null, "Mark Zuck", "mark@gmail.com");
 		User chris = new User(null, "Chris Walker", "chris@gmail.com");	
 		
-		Post post1 = new Post(null, LocalDate.parse("02/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", elon);
-		Post post2 = new Post(null, LocalDate.parse("03/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Bom dia", "Acordei feliz hoje!", elon);
-		
 		userRepository.saveAll(Arrays.asList(elon, mark, chris));
+		
+		Post post1 = new Post(null, LocalDate.parse("02/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(elon));
+		Post post2 = new Post(null, LocalDate.parse("03/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(elon));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
