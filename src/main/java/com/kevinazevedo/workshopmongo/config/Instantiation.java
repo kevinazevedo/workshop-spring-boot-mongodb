9483d2550670284f276446f49cbd4324974aa360
kevinazevedo <taylorvision95@outlook.com>
2024-05-04 19:33:20 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/kevinazevedo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/kevinazevedo/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.kevinazevedo.workshopmongo.domain.Post;
 import com.kevinazevedo.workshopmongo.domain.User;
 import com.kevinazevedo.workshopmongo.dto.AuthorDTO;
+import com.kevinazevedo.workshopmongo.dto.CommentDTO;
 import com.kevinazevedo.workshopmongo.repository.PostRepository;
 import com.kevinazevedo.workshopmongo.repository.UserRepository;
 
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, LocalDate.parse("02/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(elon));
 		Post post2 = new Post(null, LocalDate.parse("03/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(elon));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem bro!", LocalDate.parse("02/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), new AuthorDTO(mark));
+		CommentDTO c2 = new CommentDTO("Aproveite", LocalDate.parse("03/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), new AuthorDTO(chris));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", LocalDate.parse("05/05/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy")), new AuthorDTO(mark));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		elon.getPosts().addAll(Arrays.asList(post1, post2));
